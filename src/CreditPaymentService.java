f801309f1eab480754da2f69254bf382915f7865
Aleksandr Samsonov <therion@luvu.ru>
2021-07-19 15:19:13 +0300

    Some refactoring

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,7 @@
 public class CreditPaymentService {
-    public double calculate(int loanAmount, int loanTerm, double loanRate) {
+    public int calculate(int loanAmount, int loanTerm, double loanRate) {
         double monthLoanRate = loanRate / 100 / 12;
-        return (loanAmount * monthLoanRate) / (1 - (1 / Math.pow(monthLoanRate + 1, loanTerm)));
+        double annuityPayment = (loanAmount * monthLoanRate) / (1 - (1 / Math.pow(monthLoanRate + 1, loanTerm)));
+        return (int) annuityPayment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService annuityPayment = new CreditPaymentService();
-        int annuity12Month = (int) annuityPayment.calculate(1_000_000, 12, 9.99);
-        int annuity24Month = (int) annuityPayment.calculate(1_000_000, 24, 9.99);
-        int annuity36Month = (int) annuityPayment.calculate(1_000_000, 36, 9.99);
+        int annuity12Month = annuityPayment.calculate(1_000_000, 12, 9.99);
+        int annuity24Month = annuityPayment.calculate(1_000_000, 24, 9.99);
+        int annuity36Month = annuityPayment.calculate(1_000_000, 36, 9.99);
 
         System.out.println("Ежемесячный платёж (кредит на 1 год): " + annuity12Month + " ₽");
         System.out.println("Ежемесячный платёж (кредит на 2 года): " + annuity24Month + " ₽");
